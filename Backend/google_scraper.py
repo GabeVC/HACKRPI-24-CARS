f93b60a75efbc30d5f26ae787119daebc4ce5c64
DNSNate <122135017+DNSNate@users.noreply.github.com>
2024-11-09 17:50:36 -0500

    working

--- Backend/google_scraper.py
+++ Backend/google_scraper.py
@@ -60,7 +60,7 @@ def save_to_json(data, filename="google_places_with_reviews.json"):
 # Usage example
 if __name__ == "__main__":
     try:
-        data = fetch_google_places_data(query="establishment", location="42.7284,-73.6918", radius=5000)
+        data = fetch_google_places_data(query="parks", location="42.7284,-73.6918", radius=5000)
         save_to_json(data, "google_places_with_reviews.json")
         
     except requests.exceptions.HTTPError as e:
